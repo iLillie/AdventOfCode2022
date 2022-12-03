2489451b6302e46e8fea4b3d135e971fe319bb2b
Lillie Rugtveit <75392499+iLillie@users.noreply.github.com>
2022-12-03 01:53:54 +0100

    Update struct name to fit puzzle, and fancy printing

--- src/day_1.rs
+++ src/day_1.rs
@@ -2,30 +2,31 @@ use std::fs;
 
 static FILE_PATH: &str = "input/day_1.txt";
 
-pub struct DayOne {
-    pub calories: Vec<i64>,
+pub struct CalorieCounting {
+    pub elf_total_calories: Vec<i64>,
 }
 
-impl DayOne {
+impl CalorieCounting {
     pub fn init(&mut self) {
-        self.calories = DayOne::get_calories(fs::read_to_string(FILE_PATH).unwrap());
+        self.elf_total_calories =
+            CalorieCounting::get_total_elf_calories(fs::read_to_string(FILE_PATH).unwrap());
+        println!("Day 1:");
+        println!("  Calorie Counting");
     }
 
-    pub fn part_one(&mut self) {
-        let highest_calorie_count = self.calories.iter().max();
-        println!("Day 1.1: {:?}", highest_calorie_count.unwrap());
+    pub fn get_highest_total(&mut self) {
+        let highest_calorie_count = self.elf_total_calories.iter().max();
+        println!("     Highest total: {:?}", highest_calorie_count.unwrap());
     }
 
-    pub fn part_two(&mut self) {
+    pub fn get_top_three_highest_total(&mut self) {
         self.sort_calories();
-        let total_top_three: i64 = self.calories[0..3].iter().sum();
-        println!("Day 1.2: {:?}", total_top_three);
+        let total_top_three: i64 = self.elf_total_calories[0..3].iter().sum();
+        println!("     Top 3 highest total sum: {:?}", total_top_three);
     }
 
-    fn get_calories(input: String) -> Vec<i64> {
-        input
-            .split("\n\n")
-            .collect::<Vec<&str>>()
+    fn get_total_elf_calories(input: String) -> Vec<i64> {
+        CalorieCounting::get_elf_calories(input)
             .iter()
             .map(|elf_calorie| {
                 elf_calorie
@@ -38,7 +39,33 @@ impl DayOne {
             .collect::<Vec<i64>>()
     }
 
+    fn get_elf_calories(input: String) -> Vec<String> {
+        input
+            .split("\n\n")
+            .map(|input| input.to_string())
+            .collect::<Vec<String>>()
+    }
+
     fn sort_calories(&mut self) {
-        self.calories.sort_by(|a, b| b.cmp(a))
+        self.elf_total_calories.sort_by(|a, b| b.cmp(a))
+    }
+
+    pub fn print_stats(&mut self) {
+        println!(
+            "     Total entries: {:?}",
+            self.elf_total_calories.iter().count()
+        );
+        println!(
+            "     Lowest total: {:?}",
+            self.elf_total_calories.iter().min().unwrap()
+        );
+    }
+}
+
+impl Default for CalorieCounting {
+    fn default() -> Self {
+        Self {
+            elf_total_calories: vec![0],
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 mod day_1;
-use day_1::DayOne;
+use day_1::CalorieCounting;
 
 fn main() {
     run_day_one();
 }
 
 fn run_day_one() {
-    let mut day_one: DayOne = DayOne { calories: vec![0] };
-    day_one.init();
-    day_one.part_one();
-    day_one.part_two();
+    let mut calorie_counting: CalorieCounting = CalorieCounting::default();
+    calorie_counting.init();
+    calorie_counting.print_stats();
+
+    // Part 1
+    calorie_counting.get_highest_total();
+
+    // Part 2
+    calorie_counting.get_top_three_highest_total();
 }
